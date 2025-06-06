51418b9a97ca9bc517fe3954b79a018a6c4af4c6
Ricardo Ferreira <ricardo.ferreira@axonvibe.com>
2025-06-06 15:36:32 +0200

    end of lesson: disposables

--- app/src/main/java/com/example/rxandroid/MainActivity.java
+++ app/src/main/java/com/example/rxandroid/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.rxandroid;
 
+
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "RxAndroid MainActivity";
+    private String greetings = "Hello from RxJava";
+    private Observable<String> myObservable;
+    private Observer<String> myObserver;
+    private Disposable myDisposable;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,58 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // init text view from activity_main.xml
+        textView = findViewById(R.id.grettings);
+
+
+        // instantiate an Observable
+        myObservable = Observable.just(greetings);
+
+        // instruct the observable to process streaming on schedulers.io
+        myObservable.subscribeOn(Schedulers.io());
+        myObservable.observeOn(AndroidSchedulers.mainThread());
+
+
+        // instantiate an Observer
+        myObserver = new Observer<>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.i(TAG, "onSubscribe");
+                myDisposable = d;
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                Log.i(TAG, "onNext");
+                textView.setText(s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.i(TAG, "onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "onComplete");
+            }
+        };
+        // subscribe to the observable
+        myObservable.subscribe(myObserver);
+
+
+    }
+
+    // IMPORTANT NOTE:
+    // if we load things(let's say dispatch a network call) and while still loading,
+    // if the user decides to navigate back, the subscription remains there causing potentially
+    // memory leaks, ANR or app freeze. We should therefore always dispose the subscription.
+    // another way memory leak could occur is when the OS decides to kill the app or view
+    // because it need the memory for something else.
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        myDisposable.dispose();
     }
 }
